26aca101b4cf6625f93d29e7c7fa25bb3d1393ae
Gwylim Ashley <gwylim.a@gmail.com>
2016-11-25 17:27:46 +0000

    Set include_package_data=True

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(name='zhtext',
       packages=['zhtext'],
       scripts=['bin/zhtext'],
       zip_safe=False,
+      include_package_data=True,
       package_data={'zhtext': ['data/cedict_1_0_ts_utf-8_mdbg.txt', 'data/SUBTLEX-CH-WF']})
